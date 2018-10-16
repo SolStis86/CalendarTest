1281710577cc2b1e97e9e5bcde70354c8cc9b2e4
James <james@mvdigital.co>
2018-10-16 18:03:33 +0100

    Solution to solve tests

---
+++ src/Calendar.php
@@ -0,0 +1,83 @@
+<?php
+namespace Calendar;
+
+use Carbon\CarbonPeriod;
+use DateTimeInterface;
+use Carbon\Carbon;
+
+class Calendar implements CalendarInterface
+{
+    public $datetime;
+
+    public function __construct(DateTimeInterface $datetime)
+    {
+        $this->datetime = Carbon::instance($datetime);
+    }
+
+    public function getDay()
+    {
+        return $this->datetime->day;
+    }
+
+    public function getWeekDay()
+    {
+        return $this->datetime->dayOfWeekIso;
+    }
+
+    public function getFirstWeekDay()
+    {
+        return $this->datetime->clone()->startOfMonth()->dayOfWeekIso;
+    }
+
+    public function getFirstWeek()
+    {
+        return $this->datetime->clone()->startOfMonth()->weekOfYear;
+    }
+
+    public function getNumberOfDaysInThisMonth()
+    {
+        return $this->datetime->daysInMonth;
+    }
+
+    public function getNumberOfDaysInPreviousMonth()
+    {
+        return $this->datetime->clone()->startOfMonth()->subMonth()->daysInMonth;
+    }
+
+    public function getCalendar()
+    {
+        return collect($this->getCalendarPeriod())
+            ->mapToGroups(function (Carbon $day) {
+                return [$day->weekOfYear => $day];
+            })
+            ->map(function ($week) {
+                return collect($week)->mapWithKeys(function (Carbon $day) {
+                    return [$day->day => $this->shouldHighlightDay($day)];
+                });
+            })
+            ->toArray();
+    }
+
+    private function getCalendarPeriod() : CarbonPeriod
+    {
+        return CarbonPeriod::create(
+            $this->datetime->clone()->startOfMonth()->subDays($this->getFirstWeekDay() - 1),
+            '1 day',
+            $this->datetime->clone()->endOfMonth()->addDays(7 - $this->datetime->clone()->endOfMonth()->dayOfWeekIso)
+        );
+    }
+
+    private function shouldHighlightDay(Carbon $day) : bool
+    {
+        print_r([
+            'datetime' => $this->datetime->format('Y-m-d'),
+            'previous_week_of_year' => $this->datetime->weekOfYear,
+            'day' => $day->day,
+            'start_of_month_week' => $this->datetime->clone()->startOfMonth()->weekOfYear,
+            'current_day_week_of_year' => $this->datetime->weekOfYear,
+
+        ]);
+        return $day->weekOfYear !== $this->datetime->weekOfYear
+            && $day->weekOfYear === $this->datetime->clone()->subWeek()->weekOfYear;
+    }
+}
